03741a45c9229b987dd7b4e0a2c7a6580e85941a
Pra9prajapati <Pra9prajapati@gmail.com>
2023-08-23 12:00:45 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import MapComponent from './MapComponent'; 
-import geojsonData from './geojsonData.json'; 
+import React from "react";
+import MapComponent from "./MapComponent";
+import geojsonData from "./geojsonData.json";
 import "./App.css";
 
 
 function App() {
   return (
     <div className="App">
+      <h3 className="heading">GalaxEye Assessment</h3>
+      <b>
+        <p className="note">
+          Note : You can draw the shape on particular region by clicking on polygon icon located at
+          top-right corner
+        </p>
+      </b>
       <MapComponent geojsonData={geojsonData} />
     </div>
   );
